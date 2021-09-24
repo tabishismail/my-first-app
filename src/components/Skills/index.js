a32855e9ec62b2f58be84b8b2f40319f7b0f6aae
tabish1235 <tabish554@gmail.com>
2021-09-25 01:32:04 +0500

    skills page added

---
+++ src/components/Skills/index.js
@@ -0,0 +1,17 @@
+import { MyNavbar } from "../"
+import { Container, Row, Col } from "react-bootstrap"
+
+
+
+let Skills = () => {
+    return (
+
+        <Container>
+            <Row><MyNavbar /></Row>
+            <h1>Skills</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut perferendis quisquam qui. Officia, aliquid! Deleniti, eveniet fugit a aliquid magnam ea. Nobis vitae vel quidem veritatis assumenda ratione expedita sunt.</p>
+        </Container>
+    )
+}
+
+export default Skills;
