1535e9e3ee2bbc60fa260f0a4604b6f818e187bd
tabish1235 <tabish554@gmail.com>
2021-09-28 01:06:39 +0500

    responsive but navbar not responding

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import { Button, Navbar, Form, Nav, NavDropdown, FormControl } from 'react-bootstrap';
+import {Navbar, Nav,Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/index.css"
 import React from "react";
@@ -7,24 +7,42 @@ import { Link } from "react-router-dom";
 
 let MyNavbar = () => {
     return (
-        <Navbar expand="sm" className="myNavBar">
-            <Navbar.Brand className="logo" href="#">Portfolio Design</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-                <Nav
-                    className="mr-auto my-2 my-lg-0 myNav"
-                    style={{ maxHeight: '400px' }}
-                    navbarScroll
-                >
-                    <Nav.Link className="navBar" ><Link to="/" className="link">Home</Link></Nav.Link>
-                    <Nav.Link className="navBar"><Link to="/about" className="link">About</Link></Nav.Link>
-                    <Nav.Link className="navBar"><Link to="/skills" className="link">Skills</Link></Nav.Link>
-                    <Nav.Link className="navBar"><Link to="/experience" className="link">Experience</Link></Nav.Link>
-                    <Nav.Link className="navBar"><Link to="/contact" className="link">Contact</Link></Nav.Link>
-                </Nav>
+        <Navbar sticky="top" expand="sm" className="myNavBar">
+            <Container>
+                <Navbar.Brand className="logo" href="#">Portfolio Design</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"  />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link className="navBar" ><Link to="/" className="link">Home</Link></Nav.Link>
+                        <Nav.Link className="navBar"><Link to="/about" className="link">About</Link></Nav.Link>
+                        <Nav.Link className="navBar"><Link to="/skills" className="link">Skills</Link></Nav.Link>
+                        <Nav.Link className="navBar"><Link to="/experience" className="link">Experience</Link></Nav.Link>
+                        <Nav.Link className="navBar"><Link to="/contact" className="link">Contact</Link></Nav.Link>
+                    </Nav>
 
-            </Navbar.Collapse>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     )
 }
 export default MyNavbar
+
+{/* <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar> */}
